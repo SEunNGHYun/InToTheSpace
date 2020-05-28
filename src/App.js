726c74f86da904c81f50ddf39f7f4ebfdf3fcde6
윤승현 <yunseunghyeon@yunseunghyeon-ui-MacBookPro.local>
2020-05-28 20:12:44 +0900

    ✨🏇 bootstrap , Carousel, input, button 적용 bootstrap , Carousel, input, button 적용

--- src/App.js
+++ src/App.js
@@ -1,39 +1,58 @@
 import React from 'react';
 import axios from 'axios';
 import { Api }from './NASAapi';
+import {InputGroup, FormControl, Button, Carousel}from 'react-bootstrap';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      background : {}
+      background : {},
+      searchInput : ''
     }
   }
   componentDidMount() {
     axios.get(Api('planetary')+'&hd=true')
     .then(res => {
-      console.log("res", res.data);
       this.setState({ 
         background : {
           backgroundImage : `url(${res.data.hdurl})`,
-          backgroundSize : 'cover',
-          height: '790px',
+          backgroundSize : '100% 100%',
           backgroundRepeat : 'no-repeat',
-          backgroundPosition : 'center center'
+          backgroundPosition : '0% 0%',
+          height: '100vh'
           }}
         )
     })
   }
   
+  Click_Search_Button = () => {
+  }
+
+  Write_Search_Input = (e) => {
+    this.setState({
+      searchInput: e.target.value 
+    })
+  }
 
   render() {
-    console.log("state", this.state.background)
     return (
-      <div style={this.state.background}>
-        <form>
-          <input input="search"/>
-        </form>
-      </div>
+      <Carousel>
+        <Carousel.Item>
+        <div className="first-search-page" style={this.state.background}>
+            <InputGroup className="search-input" >
+              <FormControl
+                value={this.state.searchInput}
+                onChange={this.Write_Search_Input}
+                placeholder="Search for Space News"
+                />
+              <InputGroup.Append>
+                <Button variant="light" onClick={this.Click_Search_Button}>Search</Button>
+              </InputGroup.Append>
+            </InputGroup>
+        </div>
+        </Carousel.Item>
+      </Carousel>
     )
   }
 }
