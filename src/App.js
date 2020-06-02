2d093a7121faf1b07398e90682c04cbfef6cdf35
윤승현 <yunseunghyeon@yunseunghyeon-ui-MacBookPro.local>
2020-06-02 18:35:26 +0900

    ✨ SearchNews 라우터 설정 SearchNews 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Home from './Home';
-import {  BrowserRouter , Route}from 'react-router-dom';
+import SearchNews from './SearchNews';
+import { BrowserRouter , Route }from 'react-router-dom';
 
 export default class App extends Component {
     render() {
         return (
             <BrowserRouter>
                 <Route exact path="/" component={Home}/>
+                <Route path="/q/:query" component={SearchNews}/>
             </BrowserRouter>
         )
     }
